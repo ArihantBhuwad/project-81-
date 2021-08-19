02e678a628b90abae6263f5e633c51432ccc9092
ArihantBhuwad <73823807+ArihantBhuwad@users.noreply.github.com>
2021-08-19 14:55:21 +0530

    Add files via upload

---
+++ CreateStory.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import { Text,View } from 'react-native';
+
+export default class CreateStory extends Component{
+    render (){
+        retuirn (
+            <View
+            style ={{
+                flex:1,
+                justifyContent:"center",
+                alignItems:"center"           
+            }}>
+            <Text>  Create Story</Text>
+            </View>
+        )
+    }
+    
+}
+
+
+
+
+
+
+
